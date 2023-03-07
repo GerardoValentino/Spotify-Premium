692888c89665805396b825996dd61d1ecc401989
GerardoValentino <gv.rosalesramos@ugto.mx>
2023-03-06 18:30:07 -0600

    Se hizo buscador

--- js/index.js
+++ js/index.js
@@ -1,14 +1,18 @@
-const cardTop = document.querySelector('#cardTop').content
-const topTwenty = []
+const cardTop = document.querySelector('#cardTop').content    
 const fragment = document.createDocumentFragment()
 const contenido = document.querySelector('#contenido')
-let topTwoHundred = []
+const btnBuscar = document.getElementById('buscador')
+let topTwoHundred = [] // Es un arreglo porque las respuestas que llegan de APIS llegan en forma de arreglo
 
+// Cuando se cargue todo lo del diseño, imagenes, etc. Se ejecutara esto:
+// **Investigar**
 document.addEventListener('DOMContentLoaded', () => {
-    loadMusicData()
+    loadMusicData() 
 })
 
+// Creamos la funcion que acabamos de escribir:
 const loadMusicData = () => {
+    // Codigo del API que sacamos de Rapid de los Top 20
     const options = {
         method: 'GET',
         headers: {
@@ -19,20 +23,62 @@ const loadMusicData = () => {
     
     fetch('https://spotify81.p.rapidapi.com/top_200_tracks', options)
         .then(response => response.json())
-        .then(response => {
-            topTwoHundred = response
+        .then(response => { // Llaves curvas?
+            topTwoHundred = response // Guardamos la respuesta en la variable topTwoHundred
             creaCards()
-            console.log('canciones', topTwoHundred)
+            console.log('canciones', topTwoHundred) 
         })
         .catch(err => console.error(err));
 }
 
+
 const creaCards = () => {
     topTwoHundred.forEach((song) => { // Cada vez que lea un elemento del arreglo se guardara en "song"
-        cardTop.querySelector('img').setAttribute('src', song.trackMetadata.displayImageUri)
-        cardTop.querySelector('.songname').textContent = song.trackMetadata.trackName
-        const clone = cardTop.cloneNode(true)
-        fragment.appendChild(clone)
+        cardTop.querySelector('img').setAttribute('src', song.trackMetadata.displayImageUri) // Grabacion 23-02-2023 minuto 1:28:50
+        cardTop.querySelector('.songname').textContent = song.trackMetadata.trackName // trackMetadata.trackName son parte del api de rapid que elegimos
+        let artists = ''
+        let size = song.trackMetadata.artists.length
+        song.trackMetadata.artists.forEach((item, index) => {
+            if(index === size - 1){
+                artists += item.name
+            } else {
+                artists += item.name + ' / '
+            }
+        })
+        cardTop.querySelector('.artistname').textContent = artists
+
+        const clone = cardTop.cloneNode(true) // Para que sirve esto?
+        fragment.appendChild(clone) // ????
     })
-    contenido.appendChild(fragment)
+    contenido.appendChild(fragment) // ????
 }
+
+btnBuscar.addEventListener('keyup', () => {
+    const searchTerm = btnBuscar.value.toLowerCase()
+    const filteredSongs = topTwoHundred.filter((song) => {
+        return song.trackMetadata.trackName.toLowerCase().startsWith(searchTerm)
+    })
+
+    console.log('click', filteredSongs)
+    contenido.innerHTML = ''
+    if(filteredSongs.length > 0) {
+        filteredSongs.forEach((song) => { // Cada vez que lea un elemento del arreglo se guardara en "song"
+            cardTop.querySelector('img').setAttribute('src', song.trackMetadata.displayImageUri) // Grabacion 23-02-2023 minuto 1:28:50
+            cardTop.querySelector('.songname').textContent = song.trackMetadata.trackName // trackMetadata.trackName son parte del api de rapid que elegimos
+            let artists = ''
+            let size = song.trackMetadata.artists.length
+            song.trackMetadata.artists.forEach((item, index) => {
+                if(index === size - 1){
+                    artists += item.name
+                } else {
+                    artists += item.name + ' / '
+                }
+            })
+            cardTop.querySelector('.artistname').textContent = artists
+    
+            const clone = cardTop.cloneNode(true) // Para que sirve esto?
+            fragment.appendChild(clone) // ????
+        })
+        contenido.appendChild(fragment) //
+    }
+}) // Con keypress cada vez que se presiona una tecla
